b7daea37bc8fa99c4d317ca6dbac7748f19f01bf
Austin <ajamias@bu.edu>
2022-12-28 19:33:23 -0500

    Added dataset of faces. Detects face with red rectangle in window

--- opencv.py
+++ opencv.py
@@ -1,19 +1,37 @@
 import cv2 as cv
 
+# get webcam access
 cv.namedWindow("webcam")
 vc = cv.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+# train the model
+haar_cascade = cv.CascadeClassifier("faces.xml")
 
-while rval:
+if vc.isOpened():
+    read_success, frame = vc.read()
+else:
+    read_success = False
+
+while read_success:
+
+    # convert frame to greyscale
+    grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # detect and draw a red rectangle over all detected faces
+    faces_rect = haar_cascade.detectMultiScale(grey, scaleFactor=1.1, minNeighbors=3)
+    for (x,y,w,h) in faces_rect:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0,0,255), thickness=2)
+
+    # display modified frame
     cv.imshow("webcam", frame)
-    rval, frame = vc.read()
-    key = cv.waitKey(20)
-    if key == 27: # exit on ESC
+
+    # obtain next frame from webcam
+    read_success, frame = vc.read()
+
+    # exit on ESC
+    if cv.waitKey(20) == 27:
         break
 
+# disable webcam and remove all windows
 vc.release()
 cv.destroyWindow("webcam")
